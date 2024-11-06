54e66e65cbc01d37351359d313dcc32cb32fbfa6
robledo66 <robledojo250@gmail.com>
2024-11-05 23:02:02 -0300

    Agregue algunas modificaciones en los endpoints , y ordenando el proyecto

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import path from 'path';
 import peliculasRoutes from './routes/peliculasRoutes.js';
 import directoresRoutes from './routes/directoresRoutes.js';
 
 const app = express();
+const PORT = 3000;
 
 // Middleware para manejar JSON
 app.use(express.json());
+
+// Servir archivos estáticos de la carpeta 'public'
+app.use(express.static(path.join(process.cwd(), 'public')));
+
+// Ruta principal - Sirve el archivo index.html cuando accedes a '/'
+app.get('/', (req, res) => {
+    res.sendFile(path.join(process.cwd(),'parcial1', 'public', 'index.html'));
+});
 
 // Rutas de películas
 app.use('/peliculas', peliculasRoutes);
@@ -13,7 +23,6 @@ app.use('/peliculas', peliculasRoutes);
 // Rutas de directores
 app.use('/directores', directoresRoutes);
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
